f69a9ac1ad744e9e7332c6a3461683ef54509d13
havanguyen <hvnguyen902993@gmail.com>
2026-01-20 17:11:51 +0700

    feat: Implement server-side pagination and filtering for mentor and intern lists, and pass positions and departments as props to dialogs.

--- backend/src/main/java/com/rikai/backend/service/user/UserService.java
+++ backend/src/main/java/com/rikai/backend/service/user/UserService.java
@@ -20,7 +20,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,7 +31,7 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class UsersService implements IUserService {
+public class UserService implements IUserService {
     UsersRepository usersRepository;
     UserMapper userMapper;
     PasswordEncoder passwordEncoder;
